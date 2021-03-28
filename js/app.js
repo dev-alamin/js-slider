35edfa0ab1d93fc084471947dddca00506935c8d
dev-alamin <39478413+dev-alamin@users.noreply.github.com>
2021-03-28 19:42:34 +0600

    Vanilla Js Slider
    This is a verys simple slider by Native js. No Framework there

---
+++ js/app.js
@@ -0,0 +1,83 @@
+//Load Reviews Data
+const reviews = [
+    {
+        id: 1,
+        name: "Susan smith",
+        job: "web developer",
+        img: "https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type",
+    },
+    {
+        id: 2,
+        name: "Ana Johnson",
+        job: "web designer",
+        img: "https://images.unsplash.com/photo-1559138741-441acdebf148?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+        text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using",
+    },
+    {
+        id: 2,
+        name: "Another Nam",
+        job: "Intern",
+        img: "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+        text: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use",
+    },
+    {
+        id: 2,
+        name: "Peter John",
+        job: "Intern",
+        img: "https://images.unsplash.com/photo-1585316029638-334f36e92383?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80",
+        text: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use",
+    }
+];
+
+//Select Item 
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+//set starting item
+let currentItem = 0;
+
+//load inital item
+window.addEventListener("DOMContentLoaded", function () {
+    showPerson(currentItem);
+});
+
+
+//show person based on item 
+function showPerson(person) {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+
+}
+
+//show next person
+nextBtn.addEventListener("click", function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+//Previsou Button 
+prevBtn.addEventListener("click", function () {
+    currentItem--;
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
+
+//Surprise Random Button
+randomBtn.addEventListener("click", function () {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson();
+});
